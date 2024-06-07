cf82e0a0ed1ac3ec23ada7d24f2add992aad67bd
Zeno <144168045+Zeno79@users.noreply.github.com>
2024-06-07 19:24:08 +0530

    Update bot_utils.py
    Signed-off-by: Zeno <144168045+Zeno79@users.noreply.github.com>

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -136,16 +136,15 @@ def handleIndex(index, dic):
         else: break
     return index
 
-def get_progress_bar_string(pct):
-    pct = float(str(pct).strip('%'))
+def progress_bar(pct):
+    if isinstance(pct, str):
+        pct = float(pct.strip('%'))
     p = min(max(pct, 0), 100)
-    cFull = int(p // 8)
-    cPart = int(p % 8 - 1)
-    p_str = '■' * cFull
-    if cPart >= 0:
-        p_str += ['▤', '▥', '▦', '▧', '▨', '▩', '■'][cPart]
-    p_str += '□' * (12 - cFull)
-    return f"[{p_str}]"
+    cFull = int((p + 5)// 10)
+    p_str = '●' * cFull
+    p_str += '○' * (10 - cFull)
+    return p_str
+    
 
 
 def get_all_versions():
